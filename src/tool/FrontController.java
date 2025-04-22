bf68c380ed04f1a1e6389ef41a475b179e2180ba
h-inutsuka <fko2447038@stu.o-hara.ac.jp>
2025-04-22 14:13:40 +0900

    AIってすごいね

---
+++ src/tool/FrontController.java
@@ -0,0 +1,38 @@
+package tool;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("*.do")
+public class FrontController extends HttpServlet {
+    protected void doPost(HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, java.io.IOException {
+        process(req, res);
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, java.io.IOException {
+        process(req, res);
+    }
+
+    private void process(HttpServletRequest req, HttpServletResponse res)
+        throws ServletException, java.io.IOException {
+        try {
+            String path = req.getServletPath(); // 例: "/login.do"
+            String className = "scoremanager" + path.replace(".do", "Action");
+
+            Class<?> c = Class.forName(className);
+            Action action = (Action) c.getDeclaredConstructor().newInstance();
+            String nextPage = action.execute(req, res);
+
+            RequestDispatcher dispatcher = req.getRequestDispatcher(nextPage);
+            dispatcher.forward(req, res);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
